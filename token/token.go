86a76d04b9386ddf76b060603f5890199963ea67
masa-suzu <szk.masato@icloud.com>
2018-08-16 21:22:37 +0900

    Add keywords: if/else,true/false and return

--- token/token.go
+++ token/token.go
@@ -33,11 +33,21 @@ const (
 
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIndent(indent string) TokenType {
