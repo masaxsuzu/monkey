0ade54a5bda8642d0def1a5ee13130f993e2664f
masa-suzu <szk.masato@icloud.com>
2018-08-16 21:14:15 +0900

    Add tokens

--- token/token.go
+++ token/token.go
@@ -14,8 +14,14 @@ const (
 	IDENT = "IDENT"
 	INT   = "INT"
 
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
 
 	COMMA     = ","
 	SEMICOLON = ";"
