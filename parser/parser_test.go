c411b5db0dc9a9d79014d7ef2a1d8a71583aa5f1
masa-suzu <szk.masato@icloud.com>
2018-08-17 22:36:46 +0900

    Show errors by parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestStatement(t *testing.T) {
+func TestLetStatement(t *testing.T) {
 	input := `
 let x = 5;
 let y = 10;
@@ -15,6 +15,7 @@ let foobar = 838383;
 	l := lexer.New(input)
 	p := New(l)
 	program := p.ParseProgram()
+	checkParserError(t, p)
 
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
@@ -63,4 +64,16 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+func checkParserError(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
