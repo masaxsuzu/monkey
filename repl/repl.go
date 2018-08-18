1442409f909fe2df53fa80a8008f9c01efa4ab30
masa-suzu <szk.masato@icloud.com>
2018-08-18 21:59:04 +0900

    Print integer in repl

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/evaluator"
 	"monkey/lexer"
 	"monkey/parser"
 )
@@ -27,8 +28,12 @@ func Start(in io.Reader, out io.Writer, prompt string) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
