207735c48e5961bd7dbd18e71662c10bfcf60cc2
masa-suzu <szk.masato@icloud.com>
2018-08-19 02:16:44 +0900

    Evaluate function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -51,6 +51,22 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return val
 		}
 		env.Set(node.Name.Value, val)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+
+		return applyFunction(function, args)
+
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
 	}
@@ -202,6 +218,55 @@ func evalIdentifier(node *ast.Identifier,
 		return newError("identifier not found: " + node.Value)
 	}
 	return val
+}
+
+func evalExpressions(
+	exps []ast.Expression,
+	env *object.Environment) []object.Object {
+	var results []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		results = append(results, evaluated)
+	}
+
+	return results
+}
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", function.Type())
+	}
+
+	extendtedEnv := extendFunctionEnv(function, args)
+
+	evaluated := Eval(function.Body, extendtedEnv)
+
+	return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(
+	fn *object.Function,
+	args []object.Object) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+
+	return obj
 }
 
 func asTrue(obj object.Object) bool {
--- object/object.go
+++ object/object.go
@@ -1,7 +1,10 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
+	"monkey/ast"
+	"strings"
 )
 
 type ObjectType string
@@ -12,6 +15,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 type Object interface {
@@ -37,13 +41,26 @@ type Error struct {
 	Message string
 }
 
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{store: s}
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
 
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
@@ -61,8 +78,30 @@ func (obj *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
 
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ","))
+	out.WriteString(") \n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n")
+	out.WriteString(")")
+
+	return out.String()
+}
+
 func (env *Environment) Get(name string) (Object, bool) {
 	obj, ok := env.store[name]
+	if !ok && env.outer != nil {
+		obj, ok = env.outer.Get(name)
+	}
 	return obj, ok
 }
 func (env *Environment) Set(name string, obj Object) {
