622448392a3ce3af0c27cf7cfec5b76a0e3beb65
masa-suzu <szk.masato@icloud.com>
2018-08-21 02:03:45 +0900

    Add tests

---
+++ repl/repl_test.go
@@ -0,0 +1,56 @@
+package repl
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestStart(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			"\"Hello monkey!\"",
+			"Hello monkey!\n",
+		},
+		{
+			"let x = true;x;",
+			"true\n",
+		},
+
+		{
+			"let y = 2;y;",
+			"2\n",
+		},
+		{
+			"let add = fn(x,y) { return x + y ;}; add(2,3);",
+			"5\n",
+		},
+	}
+
+	for _, tt := range tests {
+		r := strings.NewReader(tt.input)
+		w := &fakeWriter{Buffer: bytes.NewBuffer(nil)}
+
+		Start(r, w, "")
+		out := w.String()
+		if out != tt.expected {
+			t.Errorf("expected=%q, got=%q", tt.expected, out)
+		}
+	}
+}
+
+type fakeWriter struct {
+	Buffer *bytes.Buffer
+}
+
+func (w *fakeWriter) Write(p []byte) (n int, err error) {
+	w.Buffer.Write(p)
+	return 0, nil
+}
+
+func (w *fakeWriter) String() string {
+	return w.Buffer.String()
+}
