407e46a0473e6e8f232a935c38cdf1a0c19a322c
masa-suzu <szk.masato@icloud.com>
2018-08-21 22:08:31 +0900

    Add colon as token

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -26,6 +26,7 @@ if (5 < 10 ){
 "foobar"
 "foo bar"
 [1,2]
+{"foo":"bar"}
 `
 	expected := []struct {
 		expectedType    token.TokenType
@@ -111,6 +112,12 @@ if (5 < 10 ){
 		{token.COMMA, ","},
 		{token.INT, "2"},
 		{token.RBRACKET, "]"},
+		{token.LBRACE, "{"},
+		{token.STRING, "foo"},
+		{token.COLON, ":"},
+		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
+
 		{token.EOF, ""}}
 	NextToken(input, expected, t)
 }
