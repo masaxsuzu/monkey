96bf5685a5523bee7f444b5829e78e8c5b601787
masa-suzu <szk.masato@icloud.com>
2018-08-21 22:03:04 +0900

    Add builtin functions

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -13,11 +13,63 @@ var builtIns = map[string]*object.Builtin{
 			}
 
 			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
 			default:
 				return newError("argument to `len` not supported, got %s", arg.Type())
 			}
+		},
+	},
+	"last": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of argument. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to last must be ARRAY, got %s", args[0].Type())
+			}
+			array := args[0].(*object.Array)
+			length := len(array.Elements)
+			if length > 0 {
+				return array.Elements[length-1]
+			}
+			return NULL
+		},
+	},
+	"first": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of argument. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to first must be ARRAY, got %s", args[0].Type())
+			}
+			array := args[0].(*object.Array)
+			length := len(array.Elements)
+			if length > 0 {
+				return array.Elements[0]
+			}
+			return NULL
+		},
+	},
+	"rest": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of argument. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to rest must be ARRAY, got %s", args[0].Type())
+			}
+			array := args[0].(*object.Array)
+			length := len(array.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length-1)
+				copy(newElements, array.Elements[1:length])
+				return &object.Array{Elements: newElements}
+			}
+			return NULL
 		},
 	},
 	"puts": &object.Builtin{
